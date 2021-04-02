d43e6c6dd206b91486ada868090215954256324b
millasml <millasml@yahoo.com.sg>
2021-04-02 23:21:19 +0800

    generated .rds file for vehicle eligibility model

--- pipeline.R
+++ pipeline.R
@@ -8,6 +8,7 @@ FIRST_NAME <- "Jane"
 
 
 # Models
+model.vehicle_eligibility <- readRDS("./home/vehicle_eligibility_LOGREG.rds")
 model.vehicle_amount <- readRDS("./vehicle/vehicle_amount_CART.rds")
 model.home_eligibility <- readRDS("./home/home_eligibility_CART.rds")
 printcp(model.home_eligibility, digits = 3)
